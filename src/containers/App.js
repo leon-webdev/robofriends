1e75a7941ad98790fbab80b80d837eb2807f0dae
Leon G. Martinez Ostos <leonma19@gmail.com>
2020-08-12 22:50:03 -0500

    agregando redux a la app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Scroll from '../components/Scroll';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundry from '../components/ErrorBoundry';
+import { setSearchField } from '../actions';
 import './App.css';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
         }
 }
 
@@ -20,16 +33,13 @@ class App extends Component {
             .then(users => this.setState({ robots: users }))
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-
     render() {
 
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
 
     const filteredRobots = robots.filter(robot => {
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
 
     return !robots.length ?
@@ -37,7 +47,7 @@ class App extends Component {
          (
             <div className='tc'>
                 <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
                         <CardList robots={ filteredRobots }/>
@@ -49,4 +59,4 @@ class App extends Component {
 
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store= {store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
