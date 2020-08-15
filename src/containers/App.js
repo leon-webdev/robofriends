7e82d9336b966b16b8544b4214627ce4896b8363
Leon G. Martinez Ostos <leonma19@gmail.com>
2020-08-15 11:31:21 -0500

    se termino de agregar redux a la app

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,45 +4,39 @@ import Scroll from '../components/Scroll';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundry from '../components/ErrorBoundry';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 import './App.css';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: []
-        }
-}
-
+    
     componentDidMount () {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }))
+        this.props.onRequestRobots();
     }
 
     render() {
 
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
 
     const filteredRobots = robots.filter(robot => {
-        return robot.name.toLowerCase().includes(searchField.toLowerCase())
-    })
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+      })
 
-    return !robots.length ?
+    return isPending ?
          <h1 className="tc">Loading</h1> :
          (
             <div className='tc'>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+const rootReducers = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware ,logger))
 
 ReactDOM.render(
   <React.StrictMode>
